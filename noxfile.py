494d3d55def346d7d0887ef0da74c0fd8d3eab13
Spacetown <michael.foerderer@gmx.de>
2021-09-19 21:23:52 +0200

    Move docker to noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,10 +1,8 @@
 import os
-import sys
+import shutil
 import nox
-from nox import sessions
-from nox.sessions import Session
 
-GCC_VERSIONS = ["gcc-5", "gcc-6", "gcc-8", "clang-10", "all"]
+GCC_VERSIONS = ["gcc-5", "gcc-6", "gcc-8", "clang-10"]
 DEFAULT_TEST_DIRECTORIES = ["doc", "gcovr"]
 BLACK_CONFORM_FILES = [
     "noxfile.py",
@@ -12,7 +10,37 @@ BLACK_CONFORM_FILES = [
     "gcovr/gcov_parser.py",
 ]
 
-nox.options.sessions = ["lint", "black", "doc", "tests(version='gcc-5')"]
+nox.options.sessions = [
+    "qa",
+    "lint",
+    "black",
+    "doc",
+    "tests(version='{}')".format(os.environ.get("CC", "gcc-5")),
+]
+
+
+def set_environment(session, cc, check=True):
+    if check and (shutil.which(cc) is None):
+        session.env["CC_REFERENCE"] = cc
+        cc = "gcc"
+        cxx = "g++"
+        gcov = "gcov"
+    else:
+        cxx = cc.replace("clang", "clang++").replace("gcc", "g++")
+        gcov = cc.replace("clang", "llvm-cov").replace("gcc", "gcov")
+    session.env["GCOVR_TEST_SUITE"] = "1"
+    session.env["CC"] = cc
+    session.env["CFLAGS"] = "--this_flag_does_not_exist"
+    session.env["CXX"] = cxx
+    session.env["CXXFLAGS"] = "--this_flag_does_not_exist"
+    session.env["GCOV"] = gcov
+
+
+@nox.session
+def qa(session):
+    for session_id in nox.options.sessions:
+        session.log("Notify session {}".format(session_id))
+        session.notify(session_id)
 
 
 @nox.session
@@ -29,12 +57,10 @@ def lint(session):
 def black(session):
     session.install("black")
     if session.posargs:
-        session.run(sys.executable, "-m", "black", *session.posargs)
+        session.run("python", "-m", "black", *session.posargs)
     else:
-        session.run(
-            sys.executable, "-m", "black", "--diff", "--check", *BLACK_CONFORM_FILES
-        )
-        session.run(sys.executable, "-m", "black", "--diff", *DEFAULT_TEST_DIRECTORIES)
+        session.run("python", "-m", "black", "--diff", "--check", *BLACK_CONFORM_FILES)
+        session.run("python", "-m", "black", "--diff", *DEFAULT_TEST_DIRECTORIES)
 
 
 @nox.session
@@ -50,27 +76,74 @@ def doc(session):
 def tests(session, version):
     session.install("-r", "requirements.txt")
     session.install("-e", ".")
-    if version == "all":
-        for version in GCC_VERSIONS:
-            if not version == "all":
-                tests(session, version)
-    else:
-        if os.environ.get("USE_COVERAGE") == "true":
-            args = ["--cov=gcovr", "--cov-branch"].append(args)
+    set_environment(session, version)
+    session.run("bash", "-c", "cd gcovr/tests && make --silent clean", external=True)
+    args = ["-m", "pytest"]
+    if os.environ.get("USE_COVERAGE") == "true":
+        args += ["--cov=gcovr", "--cov-branch"]
+    args += session.posargs
+    if "--" not in args:
+        args += ["--", "gcovr", "doc/examples"]
+    session.run("python", *args)
 
-        session.env["GCOVR_TEST_SUITE"] = "1"
-        session.env["CC"] = version
-        session.env["CFLAGS"] = "--this_flag_does_not_exist"
-        session.env["CXX"] = version.replace("clang", "clang++").replace("gcc", "g++")
-        session.env["CXXFLAGS"] = "--this_flag_does_not_exist"
-        session.env["GCOV"] = version.replace("clang", "llvm-cov").replace(
-            "gcc", "gcov"
-        )
-        session.run(
-            "bash", "-c", "cd gcovr/tests && make --silent clean", external=True
-        )
-        args = ["-m", "pytest"]
-        args += session.posargs
-        if "--" not in args:
-            args += ["--", "gcovr", "doc/examples"]
-        session.run("python", *args)
+
+@nox.session
+def tests_all_versions(session):
+    for version in GCC_VERSIONS:
+        session_id = "tests(version='{}')".format(version)
+        session.log("Notify session {}".format(session_id))
+        session.notify(session_id)
+
+
+@nox.session
+@nox.parametrize("version", GCC_VERSIONS)
+def docker_qa(session, version):
+    container_id = "gcovr-qa-{}".format(version)
+    set_environment(session, version, True)
+    session.run(
+        "bash",
+        "-c",
+        " ".join(
+            [
+                "docker",
+                "build",
+                "--tag",
+                container_id,
+                "--build-arg",
+                "USERID={}".format(os.geteuid()),
+                "--build-arg",
+                "CC=${CC}",
+                "--build-arg",
+                "CXX=${CXX}",
+                "--file",
+                "admin/Dockerfile.qa",
+                ".",
+            ]
+        ),
+        external=True,
+    )
+    session.run(
+        "bash",
+        "-c",
+        " ".join(
+            [
+                "docker",
+                "run",
+                "--rm",
+                "-e",
+                "TESTOPTS",
+                "-v",
+                "{}:/gcovr".format(os.getcwd()),
+                container_id,
+            ]
+        ),
+        external=True,
+    )
+
+
+@nox.session
+def docker_qa_all_versions(session):
+    for version in GCC_VERSIONS:
+        session_id = "docker_qa(version='{}')".format(version)
+        session.log("Notify session {}".format(session_id))
+        session.notify(session_id)
